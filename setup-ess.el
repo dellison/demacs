f02a25a5f423f0c1a2987cb0832ad27361d08dc3
David Ellison <davidehellison@gmail.com>
2014-08-05 20:27:26 -0700

    add julia location! (when i'm on the laptop)

--- setup-ess.el
+++ setup-ess.el
@@ -1,5 +1,8 @@
 (install-if-needed 'ess)
 
+(when (eq system-type 'darwin)
+  (setq inferior-julia-program-name "/Applications/Julia-0.2.1.app/Contents/Resources/julia/bin/julia-basic"))
+
 (require 'ess-site)
 
 (provide 'setup-ess)
