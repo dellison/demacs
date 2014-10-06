15ac9cec0bdacfa0c3e4284e3132990aa0a6fa4a
David Ellison <davidehellison@gmail.com>
2014-10-05 23:21:01 -0700

    some lisp improvements

--- setup-lisps.el
+++ setup-lisps.el
@@ -1,24 +1,48 @@
 ;; this is some general setup for Lisp dialects:
 ;; Emacs Lisp, Clojure, Common Lisp, Scheme
-;; 
 
-(mapc 'install-if-needed '(paredit
-			  evil-paredit
-			  rainbow-delimiters))
+(use-package paredit)
+(use-package evil-paredit)
+(use-package rainbow-delimiters)
+
+(use-package rainbow-blocks
+  :ensure rainbow-blocks)
+
+(defun de/toggle-lisp-highlighting ()
+  "Toggles between `rainbow-blocks-mode' and `rainbow-delimiters-mode'"
+  (interactive)
+  (cond
+   (rainbow-blocks-mode
+    (rainbow-blocks-mode-disable)
+    (rainbow-delimiters-mode-enable))
+   (t
+    (rainbow-blocks-mode-enable)
+    (rainbow-delimiters-mode-disable))))
 
 (require 'evil)
 (require 'paredit)
 (require 'rainbow-delimiters)
 
-(rainbow-delimiters-mode 1)
-
 (defun de/lisps-mode-hook ()
-  ;(auto-complete-mode)
+  "generalized hook for working with lisp languages"
   (rainbow-delimiters-mode 1)
-  (local-set-key (kbd "RET") 'newline-and-indent)
   (require 'evil-paredit)
   (enable-paredit-mode)
-  (evil-paredit-mode t))
+  (evil-paredit-mode t)
+  (local-set-key (kbd "C-c lh") 'de/toggle-lisp-highlighting)
+  (define-key evil-visual-state-local-map "x" 'evil-delete-char)
+  ;; (make-local-variable 'hippie-expand-try-functions-list)
+  ;; (setq hippie-expand-try-functions-list
+  ;; 	'(try-complete-file-name-partially
+  ;; 	  try-complete-file-name
+  ;; 	  try-expand-all-abbrevs
+  ;; 	  try-expand-list
+  ;; 	  try-expand-dabbrev
+  ;; 	  try-expand-dabbrev-all-buffers
+  ;; 	  try-expand-dabbrev-from-kill
+  ;; 	  try-complete-lisp-symbol-partially
+  ;; 	  try-complete-lisp-symbol))
+  )
 
 (defun de/clean-buffer-parens ()
   "Removes any space/newlines from between closing parens
@@ -31,4 +55,3 @@ in the current buffer."
       (beginning-of-line))))
 
 (provide 'setup-lisps)
-  
