72b277bae82a08f7894dcb3a92a6b7ab87704b7d
David Ellison <davidehellison@gmail.com>
2014-10-06 07:07:07 -0700

    adding eshell stuff

---
+++ setup-eshell.el
@@ -0,0 +1,13 @@
+(require 'eshell)
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
+
+(defun de/eshell-setup-hook ()
+  ""
+  (paredit-mode 1)
+  (eldoc-mode 1)
+  (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-"))))
+
+(add-hook 'eshell-setup-hook 'de/eshell-setup-hook)
