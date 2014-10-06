dcbfc7577473e6fbe9102c22fdcb0a9af9fbbd13
David Ellison <davidehellison@gmail.com>
2014-10-05 23:25:15 -0700

    dired changes

--- setup-dired.el
+++ setup-dired.el
@@ -9,15 +9,25 @@
 (require 'dired-details)
 (setq dired-details-hidden-string "--- ")
 
+(defun de/dired-find-here (wildcard)
+  (interactive "sfind " default-directory " (wildcard) :")
+  (message "i'm in " default-directory)
+  (find-name-dired default-directory wildcard)
+  )
+
 (defun de/dired-mode-hook ()
   "dired mode hook"
   (load "dired-x")
   (local-set-key (kbd "(") 'dired-details-toggle)
-  (local-unset-key (kbd "SPC")) ; so that evil-leader works
+  (local-unset-key (kbd "SPC"))		; so that evil-leader works
   (local-unset-key (kbd "M-o")) ; I prefer other-window to dired-omit-mode
-  (local-unset-key " ") ; :( it doesn't work, i want leader
+  (local-unset-key " ")		; :( it doesn't work, i want leader
+  (define-key dired-mode-map (kbd "<SPC>") nil)
+  (define-key dired-mode-map "f" 'de/dired-find-here)
   (when (fboundp 'evil-mode)
-    (define-key evil-normal-state-local-map "(" 'dired-details-toggle)))
+    (define-key evil-normal-state-local-map "(" 'dired-details-toggle)
+    (define-key evil-motion-state-local-map (kbd "0") nil)
+    (define-key evil-motion-state-local-map (kbd "RET") nil)))
 
 (add-hook 'dired-mode-hook 'de/dired-mode-hook)
 (add-hook 'dired-load-hook 'de/dired-mode-hook)
