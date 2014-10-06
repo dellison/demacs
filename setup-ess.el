5dd83faeb4c9a36b843416ceaa37a9a7d50b0729
David Ellison <davidehellison@gmail.com>
2014-10-05 23:27:30 -0700

    moving ESS to use-package

--- setup-ess.el
+++ setup-ess.el
@@ -1,7 +1,8 @@
-(install-if-needed 'ess)
-
-(when (eq system-type 'darwin)
-  (setq inferior-julia-program-name "/Applications/Julia-0.2.1.app/Contents/Resources/julia/bin/julia-basic"))
+(use-package ess
+  :ensure ess
+  :init (progn
+	  (when (eq system-type 'darwin)
+	    (setq inferior-julia-program-name "/Applications/Julia-0.2.1.app/Contents/Resources/julia/bin/julia-basic"))))
 
 (require 'ess-site)
 
