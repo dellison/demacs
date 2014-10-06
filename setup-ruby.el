6055915e70c2ca21dc1e1c93f998fb595c83e59b
David Ellison <davidehellison@gmail.com>
2014-10-05 23:30:49 -0700

    trying some ruby things

--- setup-ruby.el
+++ setup-ruby.el
@@ -1,9 +1,36 @@
-(setq de-ruby-packages '(inf-ruby))
+;; (setq de-ruby-packages '(inf-ruby))
 
-(mapc 'install-if-needed de-ruby-packages)
+;; (mapc 'install-if-needed de-ruby-packages)
 
-(require 'inf-ruby)
+(use-package inf-ruby
+  :ensure inf-ruby
+  :init (progn
+	  (defalias 'irb 'run-ruby)))
 
-(defalias 'irb 'run-ruby)
+(use-package rvm
+  :ensure rvm
+  :init (progn))
+
+(use-package projectile-rails
+  :ensure projectile-rails
+  :init (progn))
+
+(use-package robe
+  :ensure robe
+  :init (progn))
+
+(use-package web-mode
+  :ensure web-mode
+  :init (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
+
+(defun de/ruby-mode-hook ()
+  (smartparens-mode 1)
+  (robe-mode 1)
+  (projectile-on)
+  (local-set-key (kbd "C-c r a") 'rvm-activate-corresponding-ruby)
+  (local-set-key (kbd "C-c r r") 'inf-ruby)
+  )
+
+(add-hook 'ruby-mode-hook 'de/ruby-mode-hook)
 
 (provide 'setup-ruby)
