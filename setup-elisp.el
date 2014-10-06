712a2f0566e2857886eabdb38aa18d14da148522
David Ellison <davidehellison@gmail.com>
2014-10-05 23:29:36 -0700

    some elisp improvements

--- setup-elisp.el
+++ setup-elisp.el
@@ -4,9 +4,12 @@
 ;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(mapc 'install-if-needed '(elisp-slime-nav
-			   ;; litable
-			   paredit))
+;; (mapc 'install-if-needed '(elisp-slime-nav
+;; 			   ;; litable
+;; 			   paredit))
+
+(use-package elisp-slime-nav
+  :ensure elisp-slime-nav)
 
 (require 'elisp-slime-nav)
 ;; (require 'litable) ;; changed my mind about this one, i think
@@ -28,7 +31,12 @@
 ;   
 
 " "YEAH")
-(setq initial-scratch-message de/scratch-yeah)
+(setq initial-scratch-message
+      (format "; elisp scratch buffer, opened %s\n; in %s\n\n"
+	      (format-time-string "%A, %B %d, %Y at %H:%M %p")
+	      default-directory))
+
+
 
 (defun de/elisp-mode-hook ()
   "hook for emacs lisp mode"
@@ -38,22 +46,15 @@
   (litable-mode -1) ; don't use litable mode, actually...
   (paredit-mode 1)
   (smartparens-mode -1) ;; use paredit for elisp instead
-
-  ;; couldn't get this to work yet :(
-  ;; I don't want hippie-expand to use try-expand-line because
-  ;; it screws up paredit & the AST
-  ;; (set (make-local-variable hippie-expand-try-functions-list)
-  ;;      (delete 'try-expand-line 'hippie-expand-try-functions-list
-  ;; 	))
-  ;; (setq hippie-expand-try-functions-list (delete 'try-expand-line 'hippie-expand-try-functions-list))
-
-  ;; use shift-space to insert -
   (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))
+  (define-key evil-emacs-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))
   (define-key evil-normal-state-local-map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
-  (define-key evil-insert-state-local-map (kbd "TAB") 'helm-lisp-completion-at-point)
+  (define-key evil-insert-state-local-map (kbd "C-TAB") 'helm-lisp-completion-at-point)
   (turn-on-elisp-slime-nav-mode)
   (diminish 'undo-tree-mode)
-  (diminish 'yas-minor-mode))
+  (diminish 'yas-minor-mode)
+  (company-mode 1))
+
 
 (add-hook 'emacs-lisp-mode-hook 'de/elisp-mode-hook)
 (add-hook 'emacs-lisp-mode-hook 'de/lisps-mode-hook)
@@ -78,13 +79,6 @@
 	    "Hook for setting up IELM (interactive emacs lisp mode)"
 	    (litable-mode -1)
 	    (define-key evil-insert-state-local-map (kbd "DEL") 'paredit-backward-delete)))
-
-(add-hook 'eshell-setup-hook
-	  (defun de/eshell-setup-hook ()
-	    ""
-	    (paredit-mode 1)
-	    (eldoc-mode 1)
-	    (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))))
 
 (require 'setup-lisps)
 (add-hook 'emacs-lisp-mode-hook 'de/lisps-mode-hook)
