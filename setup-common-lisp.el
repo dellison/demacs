3ece05cb61bcc028fb8b0bcdadc9b41ed4cdeccb
David Ellison <davidehellison@gmail.com>
2014-10-11 12:13:55 -0700

    move slime to use-package

--- setup-common-lisp.el
+++ setup-common-lisp.el
@@ -1,13 +1,13 @@
-(install-if-needed 'slime)
-
-(when (equal system-type 'darwin)
-  (setenv "LANG" "en_US.UTF-8")
-  (setq inferior-lisp-program "/usr/local/bin/sbcl")
-  (setq slime-lisp-implementations '((sbcl ("/usr/local/bin/sbcl") :coding-system utf-8-unix)))
-  (require 'slime)
-  (slime-setup))
-
+(use-package slime
+  :ensure slime
+  :init (when (equal system-type 'darwin)
+	  (setenv "LANG" "en_US.UTF-8")
+	  (setq inferior-lisp-program "/usr/local/bin/sbcl")
+	  (setq slime-lisp-implementations '((sbcl ("/usr/local/bin/sbcl") :coding-system utf-8-unix)))
+	  (require 'slime)
+	  (slime-setup)))
 
 (add-hook 'slime-mode-hook 'de/lisps-mode-hook)
+(add-hook 'inferior-lisp-mode-hook 'de/lisps-mode-hook)
 
 (provide 'setup-common-lisp)
