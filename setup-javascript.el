ef31dd4427c47345867f811730e0a83ebf7334e1
David Ellison <davidehellison@gmail.com>
2014-11-19 02:12:06 -0800

    simple web setup

--- setup-javascript.el
+++ setup-javascript.el
@@ -1,7 +1,21 @@
-(mapc 'install-if-needed '(js2-mode))
+;; (mapc 'install-if-needed '(js2-mode))
 
-;;(require js2-mode)
+(use-package js2-mode
+  :ensure js2-mode
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    )
+  :config
+  (add-hook 'js2-mode-hook 'de/js-mode-hook)
+  (define-key js2-mode-map "{" 'paredit-open-curly)
+  (define-key js2-mode-map "}" 'paredit-close-curly-and-newline))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (use-package )
+
+(defun de/js-mode-hook ()
+  "Setup for javascript"
+  ;; (de/paredit-nonlisp)
+  )
 
 (provide 'setup-javascript)
--- setup-ruby.el
+++ setup-ruby.el
@@ -24,13 +24,25 @@
   :init (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
 
 (defun de/ruby-mode-hook ()
+  "setup for ruby files"
+  (setq sp-show-pair-from-inside 1)
   (smartparens-mode 1)
+  (show-smartparens-mode 1)
   (robe-mode 1)
   (projectile-on)
   (local-set-key (kbd "C-c r a") 'rvm-activate-corresponding-ruby)
-  (local-set-key (kbd "C-c r r") 'inf-ruby)
-  )
+  (local-set-key (kbd "C-c r r") 'inf-ruby))
 
-(add-hook 'ruby-mode-hook 'de/ruby-mode-hook)
+(add-hook 'ruby-mode-hook 'de/ruby-mode-hook nil t)
+
+(defun de/irb-mode-hook ()
+  "setup stuff for interactive ruby"
+  (setq sp-show-pair-from-inside 1)
+  (smartparens-mode 1)
+  (show-smartparens-mode 1)
+  (local-set-key (kbd "C-c r a") 'rvm-activate-corresponding-ruby)
+  (local-set-key (kbd "C-c r r") 'inf-ruby))
+
+(add-hook 'inf-ruby-mode-hook 'de/irb-mode-hook nil t)
 
 (provide 'setup-ruby)
