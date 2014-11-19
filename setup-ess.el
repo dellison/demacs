467e0e20c368255490d0343b81e6f869d7b6cdb6
David Ellison <davidehellison@gmail.com>
2014-11-19 02:09:56 -0800

    ess to use-package

--- setup-ess.el
+++ setup-ess.el
@@ -4,6 +4,4 @@
 	  (when (eq system-type 'darwin)
 	    (setq inferior-julia-program-name "/Applications/Julia-0.2.1.app/Contents/Resources/julia/bin/julia-basic"))))
 
-(require 'ess-site)
-
 (provide 'setup-ess)
