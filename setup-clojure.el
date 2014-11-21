9fd292db8223cb2f2d97a3e814258232e592b61c
David Ellison <davidehellison@gmail.com>
2014-11-20 20:35:25 -0800

    better clojure!

--- setup-clojure.el
+++ setup-clojure.el
@@ -1,27 +1,50 @@
 (unless (package-installed-p 'cider)
   (package-install 'cider))
 
+(setq cider-repl-use-pretty-printing t)
+
 (when (eq system-type 'darwin)
   (setenv "PATH" (concat (getenv "PATH") ":/Users/david/tools/leiningen")))
 
+(setq cider-repl-history-file (format "%s/.nrepl-history" demacs-directory))
+
+(defun de/clean-buffer-parens-clojure ()
+  "Removes any space/newlines from between closing parens
+in the current buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "[[:space:]\n]+\([\])]\)" nil t)
+      (replace-match "\1\2" nil nil)
+      (beginning-of-line))))
+
 (defun de/clojure-mode-hook ()
+  "setup hook for editing clojure code"
+  (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))
   (cider-turn-on-eldoc-mode)
-  (paredit-mode 1)
-  (add-hook before-save-hook 'de/clean-buffer-parens nil 'local)
-  (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-"))))
+  (define-key evil-emacs-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-"))))
 
 (defun de/cider-repl-mode-hook ()
   "setup hook run running a clojure REPL with CIDER"
   (paredit-mode 1)
-  (clojure-mode-font-lock-setup)
-  (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-"))))
+  (evil-paredit-mode 1)
+  (rainbow-delimiters-mode 1)
+  ;; (clojure-mode-font-lock-setup)
+  (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))
+  (define-key evil-emacs-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-")))
+  (cider-turn-on-eldoc-mode))
+
 
 (require 'cider)
 
+;;; I'm not sure about these:
 ;; (setq cider-repl-use-pretty-printing t
 ;;       cider-repl-use-clojure-font-lock t)
 
 (add-hook 'clojure-mode-hook 'de/clojure-mode-hook)
+(add-hook 'clojure-mode-hook 'de/lisps-mode-hook)
+
 (add-hook 'cider-repl-mode-hook 'de/cider-repl-mode-hook)
+(add-hook 'cider-repl-mode-hook 'de/lisps-mode-hook)
 
 (provide 'setup-clojure)
