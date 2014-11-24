09dce439a655e77a2beee3c3e64eebffc4697a67
David Ellison <davidehellison@gmail.com>
2014-11-24 00:40:38 -0800

    a little org setup

--- setup-org.el
+++ setup-org.el
@@ -1,14 +1,39 @@
 (require 'org)
 
-(setq org-startup-folded nil)
-(setq org-todo-keywords '((sequence "[ ]" "[X]")))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-(org-babel-do-load-languages 'org-babel-load-languages
-			     '((R . t)
-			       (emacs-lisp . t)
-			       (python . t)
-			       (ruby . t)
-			       (sh . t)
-			       ))
+(use-package org
+  :ensure org
+  :init (progn
+	  (setq org-startup-folded nil
+		org-todo-keywords '((sequence "todo" "wait" "done")))
+	  (if (eq system-type 'darwin)
+	      (setq org-directory "~/org"
+		    org-agenda-files '("~/org/todo.org"
+				       "~/org/groceries.org"
+				       "~/org/books.org"
+				       "~/org/movies.org"
+				       "~/org/lists.org"
+				       "~/org/clms.org"
+				       "~/org/records.org")
+		    org-mobile-directory "~/Dropbox/org"
+		    org-mobile-inbox-for-pull "~/org/index.org"))
+	  (org-babel-do-load-languages 'org-babel-load-languages
+				       '((emacs-lisp . t)
+					 (R . t)
+					 (python . t)
+					 (sh . t))))
+  :bind (("C-<RET>" . org-insert-heading-after-current)))
+
+(defun de/org-mode-hook ()
+  "setup for org mode"
+  (message "setting up org mode!")
+  (define-key evil-insert-state-local-map (kbd "C-<return>") 'org-insert-heading-after-current)
+  (define-key evil-emacs-state-local-map (kbd "C-<return>") 'org-insert-heading-after-current)
+  (define-key evil-normal-state-local-map (kbd "C-<return>") 'org-insert-heading-after-current)
+  (local-set-key (kbd "C-RET") 'org-insert-heading-after-current))
+
+(add-hook 'org-mode-hook 'de/org-mode-hook)
 
 (provide 'setup-org)
+  
