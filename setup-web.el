69bc5010738e6daa9614d0d2c144fc93047abe96
David Ellison <davidehellison@gmail.com>
2015-01-24 13:31:35 -0800

    adding markdown-mode

--- setup-web.el
+++ setup-web.el
@@ -1,4 +1,9 @@
 (use-package web-mode
   :ensure web-mode
+  ;; :mode "\\.html\\'"
   :config
-  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode)))
+  (progn
+    (add-to-list auto-mode-alist ("\\.html\\'"))))
+
+(use-package markdown-mode
+  :ensure markdown-mode)
