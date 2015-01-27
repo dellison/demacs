623eeb2bb47a4f730cb4cf8a780b1fb16fe33f6f
David Ellison <davidehellison@gmail.com>
2015-01-26 20:34:46 -0800

    C-w for backward-kill-word in helm

--- setup-helm.el
+++ setup-helm.el
@@ -11,13 +11,14 @@
     (require 'helm-grep)
     (require 'helm)
     (global-set-key (kbd "M-x") 'helm-M-x)
-    (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (global-set-key (kbd "C-x C-f") 'helm-for-files)
     (global-set-key (kbd "C-x b") 'helm-mini)
     (global-set-key (kbd "C-c ff") 'helm-projectile)
     (global-set-key (kbd "M-y") 'helm-show-kill-ring)
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
     (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+    (define-key helm-map (kbd "C-w") 'backward-kill-word)
     (define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
     (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
     (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
