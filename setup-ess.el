6ff0942730fdb511fb3c65d2fc0528ea50382fc9
David Ellison <davidehellison@gmail.com>
2015-04-28 12:53:51 -0700

    check before loading ESS, scheme update, and just use built-in python mode

--- setup-ess.el
+++ setup-ess.el
@@ -5,21 +5,23 @@
 (use-package ess
   :ensure ess
   :init
-  (progn
-    (when (eq system-type 'darwin)
-      (setq inferior-julia-program-name "/Users/david/tools/julia-0.3/usr/bin/julia")
-      ;; (add-to-list 'ess-tracebug-search-path "/Users/david/tools/julia-0.3/base/")
-      ))
+  (if (or (executable-find "R") (executable-find "julia"))
+      (load "ess-site"))
   :bind
   (("TAB" . ess-indent-command)
    ("S-SPC" . de/ess-ins-underscore))
   :config
-  (load "ess-site")
-  ;; (add-to-list 'ess-tracebug-search-path "/Users/david/tools/julia-0.3/base/")
-  (add-hook 'julia-mode-hook
-	    (defun de/julia-mode-hook ()
-	      (local-set-key (kbd "S-SPC") 'de/ess-ins-underscore)
-	      (local-set-key (kbd "TAB") 'ess-indent-command)))
-  (add-hook 'inferior-ess-mode-hook 'de/ess-ins-underscore))
+  (progn
+    (when (eq system-type 'darwin)
+      (setq inferior-julia-program-name "/Users/david/tools/julia-0.3/usr/bin/julia")
+      ;; (add-to-list 'ess-tracebug-search-path "/Users/david/tools/julia-0.3/base/")
+      
+      (load "ess-site")
+      ;; (add-to-list 'ess-tracebug-search-path "/Users/david/tools/julia-0.3/base/")
+      (add-hook 'julia-mode-hook
+		(defun de/julia-mode-hook ()
+		  (local-set-key (kbd "S-SPC") 'de/ess-ins-underscore)
+		  (local-set-key (kbd "TAB") 'ess-indent-command)))
+      (add-hook 'inferior-ess-mode-hook 'de/ess-ins-underscore))))
 
 (provide 'setup-ess)
--- setup-scheme.el
+++ setup-scheme.el
@@ -1,14 +1,17 @@
-(setq scheme-program-name "/usr/local/bin/scheme")
+;; (setq scheme-program-name "/usr/local/bin/scheme")
 
 (require 'xscheme)
 
 (defun de/scheme-mode-hook ()
-  (paredit-mode 1)
-  (define-key evil-normal-state-local-map (kbd "C-k") 'paredit-kill)
-  (define-key evil-insert-state-local-map (kbd "C-k") 'paredit-kill)
+  ;; (paredit-mode 1)
+  ;; (define-key evil-normal-state-local-map (kbd "C-k") 'paredit-kill)
+  ;; (define-key evil-insert-state-local-map (kbd "C-k") 'paredit-kill)
   (define-key evil-insert-state-local-map (kbd "S-SPC") (lambda () (interactive) (insert "-"))))
 
 (add-hook 'scheme-mode-hook 'de/scheme-mode-hook)
 (add-hook 'scheme-interaction-mode-hook 'de/scheme-mode-hook)
+
+(add-hook 'scheme-mode-hook 'de/lisps-mode-hook)
+(add-hook 'scheme-interaction-mode-hook 'de/lisps-mode-hook)
 
 (provide 'setup-scheme)
