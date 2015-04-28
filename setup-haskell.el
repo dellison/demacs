43df1ed1f717b8719db2469adafe6f1efc2b2c4c
David Ellison <davidehellison@gmail.com>
2015-04-28 12:24:41 -0700

    actually, don't prettify haskell after all

--- setup-haskell.el
+++ setup-haskell.el
@@ -7,7 +7,7 @@
   (progn
     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
     (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-    (setq haskell-font-lock-symbols t) ;; pretty haskell
+    (setq haskell-font-lock-symbols nil) ;; don't use pretty haskell
     (use-package inf-haskell)
     ;; (defun de/ghci ()
     ;;   "Run Haskell!"
