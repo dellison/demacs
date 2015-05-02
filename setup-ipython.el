c264283c7177f12d375454457afefcf038ec595d
David Ellison <davidehellison@gmail.com>
2015-05-02 16:15:47 -0700

    remove a couple unused files

--- setup-ipython.el
+++
@@ -1,9 +0,0 @@
-(mapc 'install-if-needed '(ein))
-
-(add-hook 'ein:connect-mode-hook
-	  (defun de/ein-connect-mode-gook ()
-	    (ein:jedi-setup)))
-
-(require 'ein)
-
-(provide 'ipython-setup)
--- setup-smex.el
+++
@@ -1,8 +0,0 @@
-(install-if-needed 'smex)
-(require 'smex)
-(smex-initialize)
-
-(when (fboundp 'smex)
-  (global-set-key (kbd "M-x") 'smex))
-
-(provide 'setup-smex)
