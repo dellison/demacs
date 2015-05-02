126c8027143f13c232b35d6367611195858f89c9
David Ellison <davidehellison@gmail.com>
2015-05-02 15:06:23 -0700

    simplified ruby setup

--- setup-ruby.el
+++ setup-ruby.el
@@ -1,32 +1,15 @@
 (use-package inf-ruby
   :ensure inf-ruby
-  :init (progn
-	  (defalias 'irb 'run-ruby)))
-
-(use-package rvm
-  :ensure rvm
-  :init (progn))
-
-(use-package projectile-rails
-  :ensure projectile-rails
-  :init (progn))
-
-(use-package robe
-  :ensure robe
-  :init (progn))
-
-(use-package web-mode
-  :ensure web-mode
-  :init (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
+  :config
+  (progn
+    (defalias 'irb 'run-ruby)))
 
 (defun de/ruby-mode-hook ()
   "setup for ruby files"
-  (setq sp-show-pair-from-inside 1)
   (smartparens-mode 1)
+  (setq sp-show-pair-from-inside 1)
   (show-smartparens-mode 1)
-  (robe-mode 1)
   (projectile-on)
-  (local-set-key (kbd "C-c r a") 'rvm-activate-corresponding-ruby)
   (local-set-key (kbd "C-c r r") 'inf-ruby))
 
 (add-hook 'ruby-mode-hook 'de/ruby-mode-hook nil t)
@@ -39,6 +22,6 @@
   (local-set-key (kbd "C-c r a") 'rvm-activate-corresponding-ruby)
   (local-set-key (kbd "C-c r r") 'inf-ruby))
 
-(add-hook 'inf-ruby-mode-hook 'de/irb-mode-hook nil t)
+(add-hook 'inf-ruby-mode-hook 'de/irb-mode-hook)
 
 (provide 'setup-ruby)
