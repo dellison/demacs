a3cc9c117dc28ffabd2f801f6816acb2ef81fd43
David Ellison <davidehellison@gmail.com>
2016-06-11 20:05:43 -0700

    rename de/open-with-eww-dwim to de/eww-dwim
    also, check for a url at point and open that if it's there

--- setup-eww.el
+++ setup-eww.el
@@ -7,7 +7,7 @@
   (let ((terms (read-from-minibuffer "Search Terms: ")))
     (eww-browse-url (concat eww-search-prefix terms))))
 
-(defun de/open-with-eww-dwim ()
+(defun de/eww-dwim ()
   "open link with EWW under point (if there's a link there),
 otherwise prompt for search terms"
   (interactive)
@@ -16,6 +16,8 @@ otherwise prompt for search terms"
     (let ((beg (region-beginning))
 	  (end (region-end)))
       (eww (buffer-substring beg end))))
+   ((thing-at-point 'url)
+    (eww-browse-url (thing-at-point 'url)))
    ((and (eq major-mode 'markdown-mode)
 	 (fboundp 'markdown-link-p) (markdown-link-p))
     (eww (markdown-link-link)))
@@ -24,6 +26,8 @@ otherwise prompt for search terms"
       (org-open-at-point)))
    (t
     (eww-search))))
+
+(global-set-key (kbd "C-c eww") #'de/eww-dwim)
 
 (defun de/eww-mode-hook ()
   "setup for EWW, the emacs web browser (wowser)"
