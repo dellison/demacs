225416d024da1464255582e1f466770ddf9944cd
David Ellison <davidehellison@gmail.com>
2016-06-22 22:11:29 -0700

    Use sharp-quotes in keybindings file
    Also use "C-c w c" for count-words in current buffer

--- setup-keybindings.el
+++ setup-keybindings.el
@@ -1,30 +1,31 @@
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-(global-set-key (kbd "C-c gr") 'lgrep)
-(global-set-key (kbd "C-c rg") 'rgrep)
-(global-set-key (kbd "C-c sw") 'swap-windows)
-(global-set-key (kbd "C-c dc") 'describe-char)
-(global-set-key (kbd "C-c rr") 'replace-rectangle)
-(global-set-key (kbd "C-c es") 'eshell)
-(global-set-key (kbd "C-c tl") 'toggle-truncate-lines)
+(global-set-key "\C-w" #'backward-kill-word)
+(global-set-key (kbd "RET") #'newline-and-indent)
+(global-set-key (kbd "C-x C-k") #'kill-region)
+(global-set-key (kbd "C-c gr") #'lgrep)
+(global-set-key (kbd "C-c rg") #'rgrep)
+(global-set-key (kbd "C-c sw") #'swap-windows)
+(global-set-key (kbd "C-c dc") #'describe-char)
+(global-set-key (kbd "C-c rr") #'replace-rectangle)
+(global-set-key (kbd "C-c es") #'eshell)
+(global-set-key (kbd "C-c tl") #'toggle-truncate-lines)
+(global-set-key (kbd "C-c wc") #'count-words)
 (global-set-key (kbd "C-c d RET") 'de/indent-to)
 ;; (global-set-key (kbd "C-c lw") 'toggle-truncate-lines)
 ;;    - "line wrap" mnemonic too
-(global-set-key (kbd "C-c ws") 'whitespace-mode)
-(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-c ws") #'whitespace-mode)
+(global-set-key (kbd "M-/") #'hippie-expand)
 
-(global-set-key "\C-ha" 'apropos) ; search for all symbols by default (instead of only commands)
-(global-set-key (kbd "C-h c") 'apropos-command)  ; previously describe-key-briefly
+(global-set-key "\C-ha" #'apropos) ; search for all symbols by default (instead of only commands)
+(global-set-key (kbd "C-h c") #'apropos-command)  ; previously describe-key-briefly
 
-(global-set-key (kbd "C-h C-f") 'describe-function)
-(global-set-key (kbd "C-h C-v") 'apropos-variable)
+(global-set-key (kbd "C-h C-f") #'describe-function)
+(global-set-key (kbd "C-h C-v") #'apropos-variable)
 
 
-(global-set-key (kbd "\C-x \C-b") 'ibuffer)
-(global-set-key (kbd "\C-x\C-m") 'execute-extended-command) ;; per Steve Yegge's advice
+(global-set-key (kbd "\C-x \C-b") #'ibuffer)
+(global-set-key (kbd "\C-x\C-m") #'execute-extended-command) ;; per Steve Yegge's advice
 
-(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "M-o") #'other-window)
 
 (defalias 'qrr 'query-replace-regexp)
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -66,15 +67,15 @@ Otherwise, moves to the beginning of the line."
   (scroll-down 1)
   (previous-line 1))
 
-;; (global-set-key (kbd "C-S-n") 'fast-move-down)
-;; (global-set-key (kbd "C-S-p") 'fast-move-up)
+;; (global-set-key (kbd "C-S-n") #'fast-move-down)
+;; (global-set-key (kbd "C-S-p") #'fast-move-up)
 
-(global-set-key (kbd "C-S-n") 'scroll-forward-1)
-(global-set-key (kbd "C-S-p") 'scroll-back-1)
+(global-set-key (kbd "C-S-n") #'scroll-forward-1)
+(global-set-key (kbd "C-S-p") #'scroll-back-1)
 
-(define-key occur-mode-map (kbd "v") 'occur-mode-display-occurrence)
-(define-key occur-mode-map (kbd "n") 'next-line)
-(define-key occur-mode-map (kbd "p") 'previous-line)
+(define-key occur-mode-map (kbd "v") #'occur-mode-display-occurrence)
+(define-key occur-mode-map (kbd "n") #'next-line)
+(define-key occur-mode-map (kbd "p") #'previous-line)
 
 (defun google-search (query)
   "Search for something on Google."
@@ -97,7 +98,7 @@ Otherwise, moves to the beginning of the line."
   (interactive)
   (let ((default-directory demacs-directory))
     (helm-projectile)))
-(global-set-key (kbd "C-c de") 'de/visit-demacs)
+(global-set-key (kbd "C-c de") #'de/visit-demacs)
 
 (when (fboundp 'evil-mode)
   (defun de/save-buffer-and-normal-state ()
@@ -105,7 +106,7 @@ Otherwise, moves to the beginning of the line."
     (interactive)
     (save-buffer)
     (evil-normal-state 1))
-  (global-set-key (kbd "C-x C-s") 'de/save-buffer-and-normal-state)
-  (define-key evil-insert-state-map (kbd "C-x C-s") 'de/save-buffer-and-normal-state))
+  (global-set-key (kbd "C-x C-s") #'de/save-buffer-and-normal-state)
+  (define-key evil-insert-state-map (kbd "C-x C-s") #'de/save-buffer-and-normal-state))
 
 (provide 'setup-keybindings)
