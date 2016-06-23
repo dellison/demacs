90e0f5e086d0563d4d24f27840c874a584c0fd67
David Ellison <davidehellison@gmail.com>
2016-06-22 23:46:28 -0700

    New command de/dired-kill-lines-or-subdir
    call with "K" in dired:
    if point is on a subdir, kill it and pop mark
    else, call dired-do-kill-lines as normal
    
    Also, bring dired keybindings out of the dired-mode-hook.

--- setup-dired.el
+++ setup-dired.el
@@ -9,9 +9,31 @@
 (require 'dired-details)
 (setq dired-details-hidden-string "--- ")
 
+(define-key dired-mode-map (kbd "<SPC>") nil)
+(define-key dired-mode-map "f" 'de/dired-find-here)
+;; use j and k for going up and down
+(define-key dired-mode-map (kbd "j") 'dired-next-line)
+(define-key dired-mode-map (kbd "k") 'dired-previous-line)
+;; (define-key dired-mode-map (kbd "K") 'dired-do-kill-lines)
+(define-key dired-mode-map (kbd "K") #'de/dired-kill-lines-or-sibdir)
+(when (fboundp 'evil-mode)
+  (define-key evil-normal-state-local-map "(" 'dired-details-toggle)
+  (define-key evil-motion-state-local-map (kbd "0") nil)
+  (define-key evil-motion-state-local-map (kbd "RET") nil))
+
 (defun de/dired-find-here (wildcard)
   (interactive "srun `find' recursively with wildcard: ")
   (find-name-dired default-directory wildcard))
+
+(defun de/dired-kill-lines-or-sibdir (&optional arg fmt)
+  ""
+  (interactive "P")
+  (let ((looking-at-subdir (dired-get-subdir)))
+    (if looking-at-subdir
+	(progn
+	  (dired-kill-subdir)
+	  (pop-global-mark))
+      (dired-do-kill-lines))))
 
 (defun de/dired-mode-hook ()
   "dired mode hook"
@@ -20,16 +42,7 @@
   (local-unset-key (kbd "SPC"))		; so that evil-leader works
   (local-unset-key (kbd "M-o")) ; I prefer other-window to dired-omit-mode
   (local-unset-key " ")		; :( it doesn't work, i want leader
-  (define-key dired-mode-map (kbd "<SPC>") nil)
-  (define-key dired-mode-map "f" 'de/dired-find-here)
-  ;; use j and k for going up and down
-  (define-key dired-mode-map (kbd "j") 'dired-next-line)
-  (define-key dired-mode-map (kbd "k") 'dired-previous-line)
-  (define-key dired-mode-map (kbd "K") 'dired-do-kill-lines)
-  (when (fboundp 'evil-mode)
-    (define-key evil-normal-state-local-map "(" 'dired-details-toggle)
-    (define-key evil-motion-state-local-map (kbd "0") nil)
-    (define-key evil-motion-state-local-map (kbd "RET") nil)))
+  )
 
 (add-hook 'dired-mode-hook 'de/dired-mode-hook)
 (add-hook 'dired-load-hook 'de/dired-mode-hook)
