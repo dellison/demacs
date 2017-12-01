e945dbd42b9db9e9c167ba434f2d510d782b55b4
David Ellison <davidehellison@gmail.com>
2017-12-01 00:45:34 -0800

    M-o in term buffers

--- setup-term.el
+++ setup-term.el
@@ -7,4 +7,6 @@
       [unspecified "#000000" "#963F3C" "#5FFB65" "#FFFD65" 
                    "#0082FF" "#FF2180" "#57DCDB" "#FFFFFF"])
 
+(define-key term-raw-map (kbd "M-o") #'other-window)
+
 (provide 'setup-term)
