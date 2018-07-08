b52465912b3c1222b4ee1c13180cb8b8ac92295f
David Ellison <davidehellison@gmail.com>
2018-07-08 07:11:07 -0700

    shift space for underscore in sml

--- setup-sml.el
+++ setup-sml.el
@@ -1,7 +1,12 @@
 (use-package sml-mode
   :ensure t
 
+  :bind (:map sml-mode-map
+	 ("S-SPC" . de/sml-underscore))
   :config
+  (defun de/sml-underscore ()
+    (interactive)
+    (insert "_"))
   (when (eq system-type 'darwin)
     (setenv "PATH" (concat (getenv "PATH") ":/usr/local/smlnj/bin"))
     (add-to-list 'exec-path "/usr/local/smlnj/bin")))
